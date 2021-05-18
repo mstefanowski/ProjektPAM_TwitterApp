a2a5ed7cba1cc68cac365da88631f8f68179218d
mstefanowski <stefanejro69@gmail.com>
2021-05-19 00:38:45 +0200

    ToggleButtons for likes and retweets with toggle Selectors, creating/destroing likes, retweeting/unretweeting

--- app/src/main/java/pl/edu/uwr/pum/twitterapp/TweetAdapter.java
+++ app/src/main/java/pl/edu/uwr/pum/twitterapp/TweetAdapter.java
@@ -1,16 +1,24 @@
 package pl.edu.uwr.pum.twitterapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Tweet;
 
 import java.util.List;
@@ -33,19 +41,93 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
         return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.usernameTextView.setText(tweetList.get(position).user.screenName);
         holder.tweetTextView.setText(tweetList.get(position).text);
 
-        String hearts = tweetList.get(position).favoriteCount.toString();
+
+        int hearts = tweetList.get(position).favoriteCount;
         int retweets = tweetList.get(position).retweetCount; //bierze followy z retweetowanego posta
 
-        holder.heartsTextView.setText(hearts);
+        holder.heartsTextView.setText(String.valueOf(hearts));
         holder.retweetsTextView.setText(Integer.toString(retweets));
 
         String imageUrl = tweetList.get(position).user.profileImageUrlHttps;
         Picasso.get().load(imageUrl).into(holder.avatarImageView);
+
+        holder.favouriteButtonToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(context, "POLAJKOWANE", Toast.LENGTH_SHORT).show();
+                    holder.heartsTextView.setText(String.valueOf(hearts + 1));
+                    TwitterCore.getInstance().getApiClient().getFavoriteService().create(tweetList.get(position).id, null).enqueue(new Callback<Tweet>() {
+                        @Override
+                        public void success(Result<Tweet> result) {
+
+                        }
+
+                        @Override
+                        public void failure(TwitterException exception) {
+
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(context, "ODLAJKOWANE", Toast.LENGTH_SHORT).show();
+                    holder.heartsTextView.setText(String.valueOf(hearts));
+                    TwitterCore.getInstance().getApiClient().getFavoriteService().destroy(tweetList.get(position).id, null).enqueue(new Callback<Tweet>() {
+                        @Override
+                        public void success(Result<Tweet> result) {
+
+                        }
+
+                        @Override
+                        public void failure(TwitterException exception) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+        holder.retweetButtonToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    holder.retweetsTextView.setText(String.valueOf(retweets + 1));
+                    TwitterCore.getInstance().getApiClient().getStatusesService().retweet(tweetList.get(position).id, null).enqueue(new Callback<Tweet>() {
+                        @Override
+                        public void success(Result<Tweet> result) {
+
+                        }
+
+                        @Override
+                        public void failure(TwitterException exception) {
+
+                        }
+                    });
+
+                } else {
+                    holder.retweetsTextView.setText(String.valueOf(retweets));
+                    TwitterCore.getInstance().getApiClient().getStatusesService().unretweet(tweetList.get(position).id, null).enqueue(new Callback<Tweet>() {
+                        @Override
+                        public void success(Result<Tweet> result) {
+
+                        }
+
+                        @Override
+                        public void failure(TwitterException exception) {
+
+                        }
+                    });
+
+                }
+            }
+        });
     }
 
     @Override
@@ -62,9 +144,13 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
 
         TextView usernameTextView, tweetTextView, heartsTextView, retweetsTextView;
         ImageView avatarImageView;
+        ToggleButton favouriteButtonToggle, retweetButtonToggle;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            favouriteButtonToggle = itemView.findViewById(R.id.favouriteToggleButton);
+            retweetButtonToggle = itemView.findViewById(R.id.retweetToggleButton);
 
             usernameTextView = itemView.findViewById(R.id.usernameTextView);
             tweetTextView = itemView.findViewById(R.id.tweetContentTextView);
