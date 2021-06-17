28e21cbbe42ab5f75540d6805b9f8ac95c86401b
mstefanowski <stefanejro69@gmail.com>
2021-06-17 22:53:30 +0200

    swapping between home and user views, added layout with users data

--- app/src/main/java/pl/edu/uwr/pum/twitterapp/TweetListActivity.java
+++ app/src/main/java/pl/edu/uwr/pum/twitterapp/TweetListActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +14,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Picasso;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +32,16 @@ public class TweetListActivity extends AppCompatActivity {
     static final int UPDATE_TWEETS = 100;
     TweetAdapter tweetAdapter;
 
+    TextView currentUserNameTextView;
+    TextView currentUserNickNameTextView;
+
+    ImageView currentUserAvatar;
+    ToggleButton modeToggleButton;
+
+    long currentUsernameID;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +50,36 @@ public class TweetListActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_view_tweet_list);
         AddTweetFAB = findViewById(R.id.AddTweetFAB);
 
+        currentUserNameTextView = findViewById(R.id.currentUserNameTextView);
+        currentUserNickNameTextView = findViewById(R.id.currentUserNicknameTextView);
+        currentUserAvatar = findViewById(R.id.currentUserAvatar);
+
+        modeToggleButton = findViewById(R.id.modeToggleButton);
+
         tweetAdapter = new TweetAdapter(this, new ArrayList<Tweet>());
         recyclerView.setAdapter(tweetAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        TwitterCore.getInstance().getApiClient().getAccountService().verifyCredentials(false, false, false).enqueue(new Callback<User>() {
+            @Override
+            public void success(Result<User> result) {
+                currentUserNameTextView.setText(result.data.screenName);
+                currentUserNickNameTextView.setText(result.data.name);
+                String uri = result.data.profileImageUrlHttps;
+                Picasso.get().load(uri).into(currentUserAvatar);
+
+                currentUsernameID = result.data.id;
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+
+            }
+        });
+
+
         updateTweets();
+
 
         AddTweetFAB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,18 +99,52 @@ public class TweetListActivity extends AppCompatActivity {
     }
 
     private void updateTweets(){
-        // pobieranie twweetów z "homeTimeline"
-        TwitterCore.getInstance().getApiClient().getStatusesService().homeTimeline(50, null, null, null, null, null, null).enqueue(new Callback<List<Tweet>>() {
-            @Override
-            public void success(Result<List<Tweet>> result) {
-                tweetAdapter.setTweetList(result.data);
-                tweetAdapter.notifyDataSetChanged();
-            }
+        if(!modeToggleButton.isChecked()){
+            // pobieranie twweetów z "homeTimeline"
+            TwitterCore.getInstance().getApiClient().getStatusesService().homeTimeline(
+                    50,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null).enqueue(new Callback<List<Tweet>>() {
+                @Override
+                public void success(Result<List<Tweet>> result) {
+                    tweetAdapter.setTweetList(result.data);
+                    tweetAdapter.notifyDataSetChanged();
+                }
 
-            @Override
-            public void failure(TwitterException exception) {
-                Log.i("stefan", exception.toString());
-            }
-        });
+                @Override
+                public void failure(TwitterException exception) {
+                    Log.i("stefan", exception.toString());
+                }
+            });
+        } else {
+            TwitterCore.getInstance().getApiClient().getStatusesService().userTimeline(currentUsernameID,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null).enqueue(new Callback<List<Tweet>>() {
+                @Override
+                public void success(Result<List<Tweet>> result) {
+                    tweetAdapter.setTweetList(result.data);
+                    tweetAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+                    Log.i("stefan", exception.toString());
+                }
+            });
+        }
+    }
+
+    public void modeToggleClicked(View view) {
+        updateTweets();
     }
 }
